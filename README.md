6a3f93aef2b6db67c2406aee6ed1b6b82ba6ff87
N.DZL <cxnt48@gmail.com>
2024-01-10 11:11:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+*Please be aware that this application / sample is provided as-is for demonstration purposes without any guarantee of support*
+=========================================================
+
+# OEM_INFO_EXERCISER (from the original code of EMDK-DeviceIdentifiers-Sample)
+Showing Zebra OEMInfo capabilities 
+
+Refer to https://techdocs.zebra.com/oeminfo/about/ for instruction about how to grant this app to access OEMInfo.
+
+To retrieve the app's signature for Stagenow get https://techdocs.zebra.com/emdk-for-android/latest/samples/sigtools/
+
+Command to get the signature:
+java -jar SigTools.jar getcert -inform apk -outform der -in app.apk -outfile app.crt
+
+
+
+
+How to access device identifiers such as serial number and IMEI on Zebra devices running Android 10
+
+Android 10 limited access to device identifiers for all apps running on the platform regardless of their target API level.  As explained in the docs for [Android 10 privacy changes](https://developer.android.com/about/versions/10/privacy/changes) this includes the serial number, IMEI and some other identifiable information.
+
+**Zebra mobile computers running Android 10 are able to access both the serial number and IMEI** however applications need to be **explicitly granted the ability** to do so and use a proprietary API.
+
+To access the serial number and IMEI file on Zebra Android devices running Android 10 or higher, first declare a new permission in your AndroidManifest.xml
+
+```xml
+<uses-permission android:name="com.zebra.provider.READ"/>
+```
+
+Then use the [MX access manager](https://techdocs.zebra.com/mx/accessmgr/) to allow your application to call the service identifiers associated with the serial number and IMEI
+
+The MX access manager settings to enable this are as follows:
+- Service Access Action: "AllowCaller" (or 'Allow Caller to Call Service')
+- Service Identifier: For the serial number use content://oem_info/oem.zebra.secure/build_serial.  For the IMEI use content://oem_info/wan/imei.  If you want to allow your app access to both, you will need to declare two different instances of the AccessManager.
+- Caller Package Name: Your package name, in the case of this sample it is com.zebra.emdk_deviceidentifiers_sample.
+- Caller Signature: The signing certificate of your application.  For more information on generating this see https://github.com/darryncampbell/MX-SignatureAuthentication-Demo.
+
+You can apply the MX access manager settings in one of three ways:
+1. Via StageNow
+2. Via your EMM
+3. Via your application, using the EMDK Profile Manager.
+
+For example, StageNow will look as follows to enable access to the serial number:
+
+![StageNow](https://github.com/darryncampbell/EMDK-DeviceIdentifiers-Sample/raw/master/screenshots/stagenow.png)
+
+You can then run this sample app and should see something like the below:
+
+![Working](https://github.com/darryncampbell/EMDK-DeviceIdentifiers-Sample/raw/master/screenshots/working.jpg)
+
+Or, on a device that does not have WAN capabilities, i.e. no SIM card or data connection: 
+
+![Non-WAN](https://github.com/darryncampbell/EMDK-DeviceIdentifiers-Sample/raw/master/screenshots/non_wan.jpg)
+
+## Handling errors:
+
+If you failed to correctly allow your application access to oem_info service, you will see an error stating so against each property you did not assign access to, as shown below:
+
+![no_service_access](https://github.com/darryncampbell/EMDK-DeviceIdentifiers-Sample/raw/master/screenshots/no_service_access.jpg)
+
+Assign access to your device and re-run the application.
+
+## Changes to Package visibility in Android 11
+
+Applications targeting SDK 30 (Android 11) will need to confirm to the package visibility filtering on Android as described in the Android documentation at [https://developer.android.com/training/package-visibility](https://developer.android.com/training/package-visibility) and [https://developer.android.com/training/package-visibility](https://developer.android.com/training/package-visibility/declaring)
+
+To grant visility by authority, add the following to your manifest:
+
+```xml
+<queries>
+    <package android:name="com.zebra.zebracontentprovider" />
+</queries>
+```
---
+++ app/src/main/java/com/zebra/oeminfo_sample/MainActivity.java
@@ -0,0 +1,103 @@
+package com.zebra.oeminfo_sample;
+
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity{
+
+    String TAG = "DeviceID";
+    String URI_SERIAL = "content://oem_info/oem.zebra.secure/build_serial";
+    String URI_IMEI = "content://oem_info/wan/imei";
+    String URI_BT_MAC = "content://oem_info/oem.zebra.secure/bt_mac";
+    String URI_WIFI_MAC = "content://oem_info/oem.zebra.secure/wifi_mac";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String resultString = "";
+
+        RetrieveOEMInfo(Uri.parse(URI_SERIAL), (TextView) findViewById(R.id.txtSerialNumber), false);       //  Build.getSerial()
+        RetrieveOEMInfo(Uri.parse(URI_IMEI), (TextView) findViewById(R.id.txtImei), true);              //  TelephonyManager getImei()
+        RetrieveOEMInfo(Uri.parse(URI_WIFI_MAC), (TextView) findViewById(R.id.txtWifiMac), false);
+
+        //  Note: Build ID and Fingerprint do not need special permission granted by Access Manager (these are standard Android IDs)
+        TextView textViewID =  findViewById(R.id.txtBuildId);
+        textViewID.setText(Build.ID);
+        TextView textViewFingerPrint =   findViewById(R.id.txtFingerprint);
+        textViewFingerPrint.setText( "App version "+BuildConfig.VERSION_NAME +"\n" + Build.FINGERPRINT+"\n"+getTargetSDK());
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void RetrieveOEMInfo(Uri uri, TextView status, boolean isIMEI) {
+        //  For clarity, this code calls ContentResolver.query() on the UI thread but production code should perform queries asynchronously.
+        //  See https://developer.android.com/guide/topics/providers/content-provider-basics.html for more information
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+
+        if (cursor == null || cursor.getCount() < 1)
+        {
+            String errorMsg = "Error: This app does not have access to call OEM service. " +
+              "Please assign access to " + uri + " through MX.  See ReadMe for more information";
+            Log.d(TAG, errorMsg);
+            status.setText(errorMsg);
+            return;
+        }
+        Log.i(TAG, "Records available =" + cursor.getCount());
+        while (cursor.moveToNext()) {
+            if (cursor.getColumnCount() == 0)
+            {
+                //  No data in the cursor.  I have seen this happen on non-WAN devices
+                String errorMsg = "Error: " + uri + " does not exist on this device";
+                Log.d(TAG, errorMsg);
+                if (isIMEI)
+                    errorMsg = "Error: Could not find IMEI.  Is device WAN capable?";
+                status.setText(errorMsg);
+            }
+            else{
+                for (int i = 0; i < cursor.getColumnCount(); i++) {
+                    Log.v(TAG, "column " + i + "=" + cursor.getColumnName(i));
+                    try {
+                        String data = cursor.getString(cursor.getColumnIndex(cursor.getColumnName(i)));
+                        Log.i(TAG, "Column Data " + i + "=" + data);
+                        status.setText(data);
+                    }
+                    catch (Exception e)
+                    {
+                        Log.i(TAG, "Exception reading data for column " + cursor.getColumnName(i));
+                    }
+                }
+            }
+        }
+        cursor.close();
+    }
+
+    String getTargetSDK(){
+        int version = 0;
+        String app_username="";
+        PackageManager pm = getPackageManager();
+        ApplicationInfo applicationInfo = null;
+        try {
+            applicationInfo = pm.getApplicationInfo(getPackageName() , 0);
+        } catch (PackageManager.NameNotFoundException e) {}
+        if (applicationInfo != null) {
+            version = applicationInfo.targetSdkVersion;
+            app_username = AndroidFileSysInfo.getNameForId( applicationInfo.uid );
+        }
+        return  "APP_TARGET_API:"+version+" APP_USER:"+app_username;
+    }
+
+}
